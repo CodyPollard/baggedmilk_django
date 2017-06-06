cd2c49b37a315c2970481003860903d44fa68671
codypollard <gowercody@gmail.com>
2017-06-05 18:19:31 -0700

    Added bootstrap. Added base.html and index.html views

--- milk/apps.py
+++ milk/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class MilkConfig(AppConfig):
-    name = 'milk'
+    name = 'milk.css'
--- milk/views.py
+++ milk/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Home Page")
+    return render(request, 'milk/index.html')
