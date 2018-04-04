d9753e234634004d066058d5411ab1b885b3b22c
Cody <codypollardeve@gmail.com>
2018-04-04 13:54:25 -0700

    Fixed some user auth css and template links

--- dnd/urls.py
+++ dnd/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^dice/$', views.dice, name='dice'),
     url(r'^shops/$', views.shops, name='shops'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
