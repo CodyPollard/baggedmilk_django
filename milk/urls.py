0b552455998db3e2a9056cf5fec98aaa2982788f
codypollard <gowercody@gmail.com>
2019-03-27 12:15:38 -0700

    Various template cleanup changes and reverted URLs change

--- milk/urls.py
+++ milk/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.wwdli, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^milkbot/$', views.milkbot, name='milkbot'),
     url(r'^timerboard/$', views.timerboard, name='timerboard'),
     url(r'pastecomparison/$', views.pastecomparison, name='pastecomparison'),
